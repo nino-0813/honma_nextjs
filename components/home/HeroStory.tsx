5144cc63ab6e0ee9a44c4d089cff9bf5e242711f
ninomiya33 <ninomiya33@users.noreply.github.com>
2026-09-15 12:22:45 +0900

    Replace homepage video and balance hero spacing

--- components/home/HeroStory.tsx
+++ components/home/HeroStory.tsx
@@ -2,7 +2,7 @@ const VIDEO = '/videos/hero.mp4';
 
 export default function HeroStory() {
   return (
-    <section className="bg-white px-4 pb-14 pt-20 md:px-8 md:pb-20 md:pt-24">
+    <section className="bg-white px-4 pb-14 pt-[calc(64px+4vw)] md:px-8 md:pb-20 md:pt-[calc(80px+5vw)]">
       <div className="mx-auto w-[92%] max-w-[1440px] overflow-hidden bg-black md:w-[90%]">
         <video
           src={VIDEO}
