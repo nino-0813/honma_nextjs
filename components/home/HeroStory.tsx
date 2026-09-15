14e2694a416024ca5a0b36da5738134f8054e754
ninomiya33 <ninomiya33@users.noreply.github.com>
2026-09-15 09:27:42 +0900

    Add more space above homepage video

--- components/home/HeroStory.tsx
+++ components/home/HeroStory.tsx
@@ -2,7 +2,7 @@ const VIDEO = '/videos/hero.mp4';
 
 export default function HeroStory() {
   return (
-    <section className="bg-white px-4 pb-14 pt-28 md:px-8 md:pb-20 md:pt-32">
+    <section className="bg-white px-4 pb-14 pt-32 md:px-8 md:pb-20 md:pt-40">
       <div className="mx-auto w-[92%] max-w-[1440px] overflow-hidden bg-black md:w-[90%]">
         <video
           src={VIDEO}
